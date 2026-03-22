f775fc7454d98e532c314c1ee401c750e1c71e30
Ritesh Yadav <deveasy44@gmail.com>
2026-03-22 22:40:26 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    float a, b;
+    char op;
+
+    printf("Enter expression (e.g. 2 + 3): ");
+    scanf("%f %c %f", &a, &op, &b);
+
+    switch(op) {
+        case '+': printf("Result = %.2f", a + b); break;
+        case '-': printf("Result = %.2f", a - b); break;
+        case '*': printf("Result = %.2f", a * b); break;
+        case '/':
+            if (b != 0)
+                printf("Result = %.2f", a / b);
+            else
+                printf("Division by zero error");
+            break;
+        default:
+            printf("Invalid operator");
+    }
+
+    return 0;
+}
